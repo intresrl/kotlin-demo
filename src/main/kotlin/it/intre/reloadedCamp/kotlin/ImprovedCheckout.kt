b2d96fa6902c06fe672bfd6f10028b496189062a
Salvi <salvi@intre.it>
2018-02-27 18:03:16 +0100

    Implementation without offers (1 failing test)

--- src/main/kotlin/it/intre/reloadedCamp/kotlin/ImprovedCheckout.kt
+++ src/main/kotlin/it/intre/reloadedCamp/kotlin/ImprovedCheckout.kt
@@ -1,11 +1,54 @@
 package it.intre.reloadedCamp.kotlin
 
 import it.intre.reloadedCamp.java.Checkout
+import kotlin.collections.Map.Entry
 
-class ImprovedCheckout: Checkout {
+const val APPLE = "apple"
+const val PEAR = "pear"
+const val PINEAPPLE = "pineapple"
+const val BANANA = "banana"
 
-    override fun pay(items: List<String>, offers: Map<String, Map.Entry<Int, Int>>): Int {
-        return 0
+class ImprovedCheckout : Checkout {
+
+    private val prices = mapOf(
+            APPLE to 50,
+            PEAR to 30,
+            PINEAPPLE to 220,
+            BANANA to 60
+    )
+
+    override fun pay(items: List<String>, offers: Map<String, Entry<Int, Int>>): Int {
+        var res = 0
+        var apples = 0
+        var pears = 0
+        var pineapples = 0
+        var bananas = 0
+
+        for (item in items) {
+            when (item) {
+                APPLE -> apples++
+                PEAR -> pears++
+                PINEAPPLE -> pineapples++
+                BANANA -> bananas++
+            }
+        }
+
+        for ((item, offer) in offers) {
+            // TODO check for offers
+        }
+
+        for ((item, price) in prices) {
+            val quantity = when (item) {
+                APPLE ->  apples
+                PEAR ->  pears
+                PINEAPPLE ->  pineapples
+                BANANA ->  bananas
+                else -> 0
+            }
+            res += quantity * price
+        }
+
+        return res
     }
 
 }
