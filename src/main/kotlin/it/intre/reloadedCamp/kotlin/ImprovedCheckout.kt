5673070f2cc81a4bd2de06766f30c53f36a84c2c
Salvi <salvi@intre.it>
2018-03-20 16:56:08 +0100

    Refactoring with streamlined method

--- src/main/kotlin/it/intre/reloadedCamp/kotlin/ImprovedCheckout.kt
+++ src/main/kotlin/it/intre/reloadedCamp/kotlin/ImprovedCheckout.kt
@@ -14,26 +14,21 @@ class ImprovedCheckout : Checkout {
             BANANA to 60
     )
 
-    override fun pay(items: List<String>, offers: Map<String, Pair<Int, Int>>): Int {
+    override fun pay(items: List<String>, offers: Map<String, Pair<Int, Int>>) =
+            items
+                    .groupBy { it }
+                    .mapValues { (_, v) -> v.size }
+                    .entries
+                    .sumBy { (item, quantity) -> payItem(offers[item], quantity, prices[item] ?: 0) }
 
-        val quantities = items
-                .groupBy { it }
-                .mapValues { (_, v) -> v.size }
-                .toMutableMap()
-
-        var offerTotal = 0
-        for ((item, offer) in offers) {
-            val (offerQuantity, offerPrice) = offer
-            val quantity = quantities[item]
-            if (quantity != null && offerQuantity <= quantity) {
-                val repeat = quantity / offerQuantity
-                offerTotal += repeat * offerPrice
-                quantities[item] = quantity - repeat * offerQuantity
+    private fun payItem(offer: Pair<Int, Int>?, quantity: Int, price: Int) =
+            when (offer) {
+                null -> quantity * price
+                else -> compute(offer, quantity, price)
             }
-        }
 
-        return offerTotal +
-                prices.entries.sumBy { (item, price) -> (quantities[item] ?: 0) * price }
+    private fun compute(offer: Pair<Int, Int>, quantity: Int, price: Int): Int {
+        val (offerQuantity, offerPrice) = offer
+        return quantity / offerQuantity * offerPrice + quantity % offerQuantity * price
     }
-
 }
